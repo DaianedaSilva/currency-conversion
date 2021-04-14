5aae63253024852c595e3e524d3b76b46e19d48d
gabrielaomoraes <gabriela.omoraes@gmail.com>
2021-04-14 14:51:43 -0300

    adding some tests

---
+++ src/main/kotlin/com/creditas/trust/currency/Money.kt
@@ -0,0 +1,23 @@
+package com.creditas.trust.currency
+
+import java.math.BigDecimal
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import kotlin.math.roundToInt
+
+class Money {
+
+
+    fun convert(s: String, s1: String): String {
+        val tax = BigDecimal(6.7170).setScale(3, RoundingMode.HALF_EVEN)
+        val value = BigDecimal(100)
+        val result = tax.multiply(value)
+
+        val dec = DecimalFormat("####.##")
+
+
+        return dec.format(result)
+
+    }
+
+}
--- src/test/kotlin/com/creditas/trust/currency/MoneyTest.kt
+++ src/test/kotlin/com/creditas/trust/currency/MoneyTest.kt
@@ -1,28 +1,37 @@
-//package com.creditas.trust.currency
-//
-//import org.hamcrest.CoreMatchers.equalTo
-//import org.junit.Assert.assertThat
-//import org.junit.Test
-//import java.math.BigDecimal
-//
-//class MoneyTest {
-//
+package com.creditas.trust.currency
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThat
+import org.junit.Test
+import java.math.BigDecimal
+
+class MoneyTest {
+
+    @Test
+    fun `Converts 100 BRL to EUA currency`(){
+        val result = Money().convert("BRL100", "EUA")
+
+        assertEquals("671.70", result)
+    }
+
 //    @Test
 //    fun `adds its own amount to a big decimal`() {
 //        val twentyTwo = Money("22.00")
 //
-//        val result = twentyTwo + BigDecimal("22.00")
+//        //val result = twentyTwo + BigDecimal("22.00")
 //
-//        assertThat(result, equalTo(Money("44.00")))
+//        //assertThat(result, equalTo(Money("44.00")))
 //    }
+//
 //
 //    @Test
 //    fun `adds to another money`() {
 //        val ten = Money("10.00")
 //        val five = Money("5.00")
 //
-//        val result = ten + five
+//        //val result = ten five
 //
-//        assertThat(result, equalTo(Money("15.00")))
+//        //assertThat(result, equalTo(Money("15.00")))
 //    }
-//}
+}
