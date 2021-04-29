d97e67f30f4d1b9bf6eb69725c524dadfe39b5b4
Daiane <daianedasilva044@gmail.com>
2021-04-29 18:54:29 -0300

    Novo projeto criado com Spring e MongoDB

---
+++ src/test/kotlin/com/creditas/trust/currencyconversion/CurrencyConversionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.creditas.trust.currencyconversion
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CurrencyConversionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
